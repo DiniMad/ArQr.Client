46e79fabce4c003e2c649f07f449c32b3af3054d
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-01-25 19:10:34 +0330

    Add the global style component to the app component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import GlobalStyles from "./utils/GlobalStyles";
 
 function App() {
     return (
-        <div className="App">
-     
-        </div>
+        <>
+            <GlobalStyles/>
+        </>
     );
 }
 
