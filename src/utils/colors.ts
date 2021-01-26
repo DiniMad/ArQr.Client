a051c613179b12aa0d788ca7119a9d1d44386eab
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-01-26 14:17:05 +0330

    Create the "colors" and "themes" files.

---
+++ src/utils/colors.ts
@@ -0,0 +1,18 @@
+const black = {
+    100: "#121212",
+    200: "#1f1f1f",
+    300: "#333333",
+}
+
+const purple = {
+    100: "#3a174a",
+    200: "#732991",
+    300: "#963dbf",
+}
+
+const colors = {
+    purple,
+    black
+}
+
+export default colors;
---
+++ src/utils/themes.ts
@@ -0,0 +1,16 @@
+import colors from "./colors"
+
+const dark = {
+    backgroundDark: colors.black[100],
+    background: colors.black[200],
+    backgroundLight: colors.black[300],
+    primaryDark: colors.purple[100],
+    primary: colors.purple[200],
+    primaryLight: colors.purple[300],
+}
+
+const themes = {
+    dark
+}
+
+export default themes;
