4e9c38555cda4a4d6e3aece5736adb685750f0b6
Seyed Hossein Hosseini <payoff900@gmail.com>
2021-01-31 21:15:45 +0330

    Create a method to choose the correct component based on the window size.

--- src/utils/mediaQueries.ts
+++ src/utils/mediaQueries.ts
@@ -1,3 +1,11 @@
+import {FunctionComponent} from "react";
+
 export const mediaQueries = {
     mobileMaxWidth: 480
+}
+
+export const sizeBasedComponent = (desktopSizeComponent: FunctionComponent, mobileSizeComponent: FunctionComponent) => {
+    const width = window.innerWidth;
+    const isDesktopSize = width > mediaQueries.mobileMaxWidth;
+    return  isDesktopSize ? desktopSizeComponent : mobileSizeComponent;
 }
